89995d7f3fc11a549a68860a6afba4b46721598d
JyotiSwain <jswain.lpu@gmail.com>
2022-07-05 13:35:52 +0530

    3.Working of Navbar and CSS using JS objects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import {icons} from "../styles/global";
+import {icon, links, logo, navbar, signs} from "../styles/homeStyle";
 
-function Navbar() {
+
+const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav style={navbar}>
+      <div style = {logo}>
+        <h1>MR</h1>
+      </div>
+
+      <div style={links}>
+        <span className="link">Vectors</span>
+        <span className="link">Photos</span>
+        <span className="link">PSD</span>
+        <span className="link">Video</span>
+        <span className="link">More</span>
+      </div>
+
+      <div style={signs}>
+        <span style={icon}>{icons.mail}</span>
+        <span style={icon}>{icons.facebook}</span>
+      </div>
+    </nav>
   )
 }
 
---
+++ src/styles/homeStyle.js
@@ -0,0 +1,39 @@
+import { FaCentercode } from 'react-icons/fa';
+import {colors} from './global'
+
+export const layout = {
+    padding:"2rem 4rem",
+    backgroundColor:colors.grey,
+    color:colors.dark,
+    minHeight: "100vh",
+};
+
+export const navbar = {
+    display:"flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+
+}
+
+export const logo = {
+    backgroundColor:colors.yellow,
+    borderRadius:"20%",
+    width:"50px",
+    height:"50px",
+    display:"flex",
+    alignItems:"center",
+    justifyContent:"center",
+}
+
+export const signs = {
+    display:"flex",
+}
+
+export const links = {
+    display:"flex",
+    columnGap:"3rem",
+}
+
+export const icon = {
+
+}
